c0dff583a18147e92b52628e3b1725a87f1a5871
Matan Kedar <kedar.matan@outlook.com>
2023-07-25 22:57:22 +0300

    Added ceo name generation to be used in the future for news articles

---
+++ routes/profile.tsx
@@ -0,0 +1,13 @@
+import Profilepage from '../islands/Profilepage.tsx'
+import Header from "../components/Header.tsx";
+
+export default function Profile() {
+  return (
+    <>
+      <Header />
+      <Profilepage />
+    </>
+  )
+}
+
+
