f630f67d71ab7b20dc75821970e1ed6fd626c38f
Matan Kedar <kedar.matan@outlook.com>
2023-07-26 14:39:42 +0300

    added articles folder and started development on articleGeneration.ts

---
+++ generation/articlesGeneration/executiveNameGeneration.ts
@@ -0,0 +1,59 @@
+export function generateRandomName(): string {
+  const firstNames = [
+    "Aarav",
+    "Emiko",
+    "Hassan",
+    "Lina",
+    "Santiago",
+    "Leila",
+    "Hiroshi",
+    "Aisha",
+    "Rahul",
+    "Yara",
+    "Abdi",
+    "Zara",
+    "Mohammed",
+    "Chen",
+    "Ananya",
+    "Ahmed",
+    "Mei",
+    "Omar",
+    "Liyana",
+    "Yuval",
+    "Noam",
+    "Lex",
+    "Ido",
+  ];
+
+  const lastNames = [
+    "Garcia",
+    "Silva",
+    "Kim",
+    "Khan",
+    "Santos",
+    "Mwangi",
+    "Singh",
+    "Abbas",
+    "Fernandez",
+    "Nguyen",
+    "Ali",
+    "Choi",
+    "Ahmed",
+    "Yamamoto",
+    "Zhang",
+    "Das",
+    "Chung",
+    "Rahman",
+    "Sato",
+    "Fridman",
+    "Jones",
+    "Nurmagomedov",
+  ];
+
+  const randomFirstName =
+    firstNames[Math.floor(Math.random() * firstNames.length)];
+  const randomLastName =
+    lastNames[Math.floor(Math.random() * lastNames.length)];
+
+  return `${randomFirstName} ${randomLastName}`;
+}
