3ba1efb22662359646d4769fced5e90dcafb32df
bdeweesevans <bdeweesevans@gmail.com>
2023-08-12 16:41:14 -0700

    landing page & rename of adminpanel

---
+++ routes/landingpage.tsx
@@ -0,0 +1,13 @@
+import LandingPage from "../islands/LandingPage.tsx";
+import Header from "../islands/Header.tsx";
+import Footer from "../components/Footer.tsx";
+
+export default function JoinClassPage() {
+  return (
+    <>
+      <Header />
+      <LandingPage />
+      <Footer />
+    </>
+  );
+}
