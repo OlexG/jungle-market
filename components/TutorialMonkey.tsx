b628771b1e7fc4279c3c97b757d216e890d5584b
Matan Kedar <kedar.matan@outlook.com>
2023-08-16 18:07:22 -0700

    Forward and backwards button in tutorial monkey

--- components/TutorialMonkey.tsx
+++ components/TutorialMonkey.tsx
@@ -6,15 +6,21 @@ interface IProps {
     setPreviousPanel: () => void
 }
 export default function TutorialMonkey(props: IProps) {
-  const imageArray: string[] = [
+  
+
+    const imageArray: string[] = [
     "DALL_E_2023-08-15_11.28.01_-_Please_open_the_monkies_mouth-removebg-preview.png", // closed mouth
     "ilikebirds82_a_high_quality_picture_of_a_cartoon_monkey_with_gl_7dfb7f53-be0d-453d-b916-7a3475cebed7-removebg-preview.png", // open mouth
     "DALL_E_2023-08-15_11.30.12_-_Please_close_the_monkies_eyes-removebg-preview.png", // blink
   ];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
   const [isTalking, setIsTalking] = useState(false);
+  const { texts } = props;
+  const [currentArrayText, setArrayCurrentText] = useState(0);
+  const [text, setText] = useState(texts[currentArrayText]);
+  const [displayText, setDisplayText] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const turnOnTalking = (numberOfCharacters: number) => {
     const duration = numberOfCharacters * 0.05 * 1000;
@@ -25,19 +31,13 @@ export default function TutorialMonkey(props: IProps) {
     }, duration);
   };
 
-    const { texts } = props;
-
   useEffect(() => {
-    const numberOfCharacters = texts[0].length;
+    const numberOfCharacters = texts[currentArrayText].length;
 
     if (numberOfCharacters > 0) {
       turnOnTalking(numberOfCharacters);
     }
-  }, []);
-
-  const [text, setText] = useState(texts[0]);
-  const [displayText, setDisplayText] = useState("");
-  const [currentIndex, setCurrentIndex] = useState(0);
+  }, [currentArrayText]);
 
   useEffect(() => {
     const typingInterval = setInterval(() => {
@@ -53,32 +53,40 @@ export default function TutorialMonkey(props: IProps) {
   }, [currentIndex, text]);
 
   useEffect(() => {
-    if (isTalking) {
-      const intervalId = setInterval(() => {
-        const randomIndex = Math.floor(Math.random() * 2);
-        setCurrentImageIndex(randomIndex);
-      }, getRandomInterval(150, 250));
-  
-      return () => {
-        clearInterval(intervalId);
-      };
-    }
-  }, [isTalking]);
+    const intervalId = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * 2);
+      setCurrentImageIndex(randomIndex);
+    }, getRandomInterval(150, 250));
 
-  useEffect(() => {
     const blinkIntervalId = setInterval(() => {
-      setCurrentImageIndex(2); 
+      setCurrentImageIndex(2);
       setTimeout(() => {
-        setCurrentImageIndex(1); 
+        setCurrentImageIndex(1);
       }, 500);
     }, 15000);
-  
+
     return () => {
+      clearInterval(intervalId);
       clearInterval(blinkIntervalId);
     };
   }, []);
-  
-    
+
+  const pageForward = () => {
+    setDisplayText("");
+    setCurrentIndex(0); 
+    setArrayCurrentText(currentArrayText + 1);
+    setText(texts[currentArrayText + 1]);
+    props.setNextPanel();
+  };
+
+  const pageBackward = () => {
+    setDisplayText("");
+    setCurrentIndex(0);
+    setArrayCurrentText(currentArrayText - 1);
+    setText(texts[currentArrayText - 1]); 
+    props.setPreviousPanel();
+  };
+
   const getRandomInterval = (min: number, max: number) => {
     return Math.random() * (max - min) + min;
   };
@@ -95,10 +103,16 @@ export default function TutorialMonkey(props: IProps) {
         style={{ backgroundColor: "rgba(0, 0, 0, 0.9)" }}
       >
         <div className="flex space-x-4">
-          <button className="w-8 h-8 flex items-center justify-center bg-white rounded-full hover:bg-gray-200 mt-3 ml-2">
+          <button className="w-8 h-8 flex items-center justify-center bg-white rounded-full hover:bg-gray-200 mt-3 ml-2"
+            onClick={pageBackward}
+
+          
+          >
             &#8592; 
           </button>
-          <button className="w-12 h-12 flex items-center justify-center bg-white rounded-full hover:bg-gray-200">
+          <button className="w-12 h-12 flex items-center justify-center bg-white rounded-full hover:bg-gray-200"
+               onClick={pageForward}>
+            
             &#8594;
           </button>
         </div>
