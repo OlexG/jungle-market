df8b2b88dd7a77b75cee5e3cd70360526774da36
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2023-09-25 13:13:36 -0700

    test

--- components/Paginate.tsx
+++ components/Paginate.tsx
@@ -19,6 +19,7 @@ export default function Paginate(props: IProps) {
   };
 
   let startPage = currentPage - Math.floor(props.pageRangeDisplayed / 2);
+  console.log(startPage)
   if (startPage < 1) startPage = 1;
   if (startPage + props.pageRangeDisplayed - 1 > props.pageCount) {
     startPage = props.pageCount - props.pageRangeDisplayed + 1;
